ddeae85dfd96cbef6589788ef034b2f5047a0ae9
Benjamin <bnjackson951@gmail.com>
2023-03-18 20:07:08 +0000

    Add functionality for user to be able to change section by clicking on controls

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 let allTasks = [];
 let uncompletedTasks = [];
 let completedTasks = [];
-let submitButton = document.getElementById('task-submit');
 let allTasksContainer = document.getElementById('all-tasks');
 let uncompletedTasksContainer = document.getElementById('uncompleted-tasks');
-let completedTasksContainer = document.getElementById('completedTasks');
+let completedTasksContainer = document.getElementById('completed-tasks');
 
-submitButton.addEventListener('click', () => {
+document.getElementById('task-submit').addEventListener('click', () => {
     let input = document.getElementById('task-input');
     if(allTasks.length === 0) {
         clearTasks();
@@ -25,7 +24,6 @@ function clearTasks() {
     while (allTasksContainer.firstChild) {
         allTasksContainer.removeChild(allTasksContainer.firstChild);
     }
-    
 }
 
 function createTask() {
@@ -40,5 +38,27 @@ function createTask() {
         allTasksContainer.append(taskWrapper);
         let taskWrapperClone = taskWrapper.cloneNode(true);
         uncompletedTasksContainer.append(taskWrapperClone);
+    }
+}
+
+
+document.getElementById('clear-btn').addEventListener('click', clearTasks);
+document.getElementById('all').addEventListener('click', changeSection);
+document.getElementById('uncompleted').addEventListener('click', changeSection);
+document.getElementById('completed').addEventListener('click', changeSection);
+
+function changeSection() {
+    if(event.target.id === 'all') {
+        allTasksContainer.style.display = 'block';
+        uncompletedTasksContainer.style.display = 'none';
+        completedTasksContainer.style.display = 'none';
+    } else if (event.target.id === 'uncompleted') {
+        uncompletedTasksContainer.style.display = 'block';
+        allTasksContainer.style.display = 'none';
+        completedTasksContainer.style.display = 'none';
+    } else if (event.target.id === 'completed') {
+        completedTasksContainer.style.display = 'block';
+        allTasksContainer.style.display = 'none';
+        uncompletedTasksContainer.style.display = 'none';
     }
 }
