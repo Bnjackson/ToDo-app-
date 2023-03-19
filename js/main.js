c82bbe767870a287b651d66b17b5369be87ec750
Benjamin <bnjackson951@gmail.com>
2023-03-19 17:10:50 +0000

    Add function changeSelected that will make bold the current section

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-let allTasks = [];
-let uncompletedTasks = [];
-let completedTasks = [];
-let allTasksContainer = document.getElementById('all-tasks');
-let uncompletedTasksContainer = document.getElementById('uncompleted-tasks');
-let completedTasksContainer = document.getElementById('completed-tasks');
+const allTasks = [];
+const uncompletedTasks = [];
+const completedTasks = [];
+const allTasksContainer = document.getElementById('all-tasks');
+const uncompletedTasksContainer = document.getElementById('uncompleted-tasks');
+const completedTasksContainer = document.getElementById('completed-tasks');
 
 document.getElementById('task-submit').addEventListener('click', () => {
     let input = document.getElementById('task-input');
@@ -24,6 +24,12 @@ function clearTasks() {
     while (allTasksContainer.firstChild) {
         allTasksContainer.removeChild(allTasksContainer.firstChild);
     }
+    while (uncompletedTasksContainer.firstChild) {
+        uncompletedTasksContainer.removeChild(uncompletedTasksContainer.firstChild);
+    }
+    while (completedTasksContainer.firstChild) {
+        completedTasksContainer.removeChild(completedTasksContainer.firstChild);
+    }
 }
 
 function createTask() {
@@ -42,23 +48,44 @@ function createTask() {
 }
 
 
-document.getElementById('clear-btn').addEventListener('click', clearTasks);
-document.getElementById('all').addEventListener('click', changeSection);
-document.getElementById('uncompleted').addEventListener('click', changeSection);
-document.getElementById('completed').addEventListener('click', changeSection);
+const clearButton = document.getElementById('clear-btn');
+const allSection = document.getElementById('all');
+const uncompletedSection = document.getElementById('uncompleted');
+const completedSection = document.getElementById('completed');
+
+clearButton.addEventListener('click', clearTasks);
+allSection.addEventListener('click', changeSection);
+uncompletedSection.addEventListener('click', changeSection);
+completedSection.addEventListener('click', changeSection);
 
 function changeSection() {
     if(event.target.id === 'all') {
         allTasksContainer.style.display = 'block';
         uncompletedTasksContainer.style.display = 'none';
         completedTasksContainer.style.display = 'none';
+        changeSelected(event.target.id);
     } else if (event.target.id === 'uncompleted') {
         uncompletedTasksContainer.style.display = 'block';
         allTasksContainer.style.display = 'none';
         completedTasksContainer.style.display = 'none';
+        changeSelected(event.target.id);
     } else if (event.target.id === 'completed') {
         completedTasksContainer.style.display = 'block';
         allTasksContainer.style.display = 'none';
         uncompletedTasksContainer.style.display = 'none';
+        changeSelected(event.target.id);
+    }
+    function changeSelected(section) {
+        allSection.classList.remove('selected');
+        uncompletedSection.classList.remove('selected');
+        completedSection.classList.remove('selected');
+        if(section === 'all') {
+            allSection.classList.add('selected');
+        } else if (section === 'uncompleted') {
+            uncompletedSection.classList.add('selected');
+        } else if (section === 'completed') {
+            completedSection.classList.add('selected');
+        }
+        section.classList.add('selected');
     }
 }
